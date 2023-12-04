39a8fc8189f3354abe6dd079c447ea05be282d3e
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-04 19:31:35 +0300

    can't get this file to run

--- lib/debug.py
+++ lib/debug.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-from config import CONN, CURSOR
-from song import Song
+from lib import CONN, CURSOR
+from lib.song import Song
 
-
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+import pytest; pytest.set_trace()
